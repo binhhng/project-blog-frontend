00a5f46f0e3376257b1455e6a4e21b843a2161a1
m2mforever <nevermind271999@gmail.com>
2020-04-26 19:42:51 +0700

    Create main Menu

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,11 +1,15 @@
 // eslint-disable
 import React, { useContext } from 'react'
 import { CommonContext } from '@tools'
+import { Menu, Select} from 'antd';
+import { HomeTwoTone, UserOutlined, GlobalOutlined, PoweroffOutlined, SearchOutlined } from '@ant-design/icons';
+
 
 function Layout(props) {
   const { history, t } = props
+  const { Header, Content, Footer } = Layout
   const { dispatch } = useContext(CommonContext)
-
+  
   function onHandleChangeLanguage(e) {
     const { value } = e.target
     dispatch({ type: "changeLanguage", payload: value })
@@ -19,21 +23,31 @@ function Layout(props) {
     console.log('logout')
     dispatch({ type: "logout", payload: false })
   }
+  
 
   return (
     <div>
-      <select onChange={onHandleChangeLanguage}>
-        <option value="vi">{t('option.changeLang')}</option>
-        <option value="vi">{t('common.language.vietnamese')}</option>
-        <option value="en">{t('common.language.english')}</option>
-      </select>
-      <ul>
-        <li onClick={() => onChangeRoute('/')}>{t('common.menu.home')} (click)</li>
-        <li onClick={() => onChangeRoute('/role')}>{t('common.menu.role')} (click)</li>
-        <li onClick={onLogout}>{t('common.menu.logout')}</li>
-      </ul>
-      <p>this is {props.children} page</p>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1"><div className="HomeLogo"/><HomeTwoTone />Home</Menu.Item>
+        <Menu.Item key="2"><div className="UserLogo"/><UserOutlined />User</Menu.Item>
+        <Menu.Item key="3"><div className="SearchLogo"/><SearchOutlined />Search</Menu.Item>
+        <Menu.Item key="4"><div className="LogoutLogo"/>< PoweroffOutlined />
+        <button onChange={onLogout}>Logout
+        </button> 
+        </Menu.Item>
+        <Menu.Item key="5"><div className="LanguageLogo"/><GlobalOutlined />
+        <select onChange={onHandleChangeLanguage}>
+          <option value="vi">ChangeLanguage</option>
+          <option value="vi">Vietnamese</option>
+          <option value="en">English</option>
+        </select> 
+        </Menu.Item>
+
+      </Menu>  
     </div>
+
+ 
   )
 }
 
